2986536c3db8ee7dc31177664993de75517fbd53
dpfinch <d.finch@ed.ac.uk>
2017-11-02 12:56:15 +0000

    Started a function that selects just one variable from all data dataframe

--- open_DEFRA_csv.py
+++ open_DEFRA_csv.py
@@ -3,12 +3,14 @@
 # e.g. https://uk-air.defra.gov.uk/data/
 # Function Names:
 #       open_csv(filename, skip_num_rows = 4)
+#       select_one_variable(variablename, filename = )
 #==============================================================================
 # Uses modules:
-# datetime, numpy, pandas
+# datetime, numpy, pandas, os, sys
 from datetime import datetime, timedelta
 import pandas as pd
 import numpy as np
+import os, sys
 #==============================================================================
 def open_csv(filepath, skip_num_rows = 4):
     """
@@ -19,7 +21,7 @@ def open_csv(filepath, skip_num_rows = 4):
         Has numerous 'Status' columns - as in status for each measurements,
         these are numbered Status, Status.1, Status.2 etc.
         Function IN:
-            filepath (REQUIRED. STRING):
+            filepath (REQUIRED, STRING):
                     path and name of csv file (eg /home/user/myfiles/AirQuality.csv)
             skip_num_rows(DEFAULT = 4, INTEGER):
                     Number of lines to skip of the file, usually 4 for DEFRA
@@ -57,6 +59,36 @@ def open_csv(filepath, skip_num_rows = 4):
 
     return df
 
+def select_one_variable(variablename, filename = 'ExampleData'):
+    """
+        This function reduces the entire data set down to just the species/
+        variable that is wanted for a particular analysis. eg. just NO2.
+        Function IN:
+            variablename(REQUIRED, STRING):
+                The name of the species (eg. Nitrogen dioxide)
+            filename(OPTIONAL, STRING):
+                The path and name of the file. If not chosen then automatically
+                uses the example data.
+        Function OUT:
+            species_data:
+                A reduced pandas DataFrame that contains the date and time,
+                species concentration, the unit, and the measurement validity
+    """
+
+    # If the filename is not set, then use the example data provided
+    if filename == 'ExampleData':
+        filename = 'Example_Data/edinburgh_st_leonards_2015_2017.csv'
+        print "No file specified. Using data from: %s" % filename
+
+    # Check to see whether file  & path exists
+    # If it doesn't exist, print warning and exit
+    if not os.path.exists(filename):
+        print "Unable to open %s \n File doesn't exist."
+        sys.exit()
+
+
+    all_data = open_csv(filename)
+    return species_data
 
 if __name__ == '__main__':
     filename  = 'Example_Data/' \
