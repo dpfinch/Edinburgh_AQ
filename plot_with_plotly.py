c3f04bffcc7c992e5d43a322340d4149e4eaab08
dpfinch <d.finch@ed.ac.uk>
2017-11-02 11:58:06 +0000

    Created skeleton code to produce plots through plot.ly

---
+++ plot_with_plotly.py
@@ -0,0 +1,34 @@
+#==============================================================================
+# Do simple plots through plot.ly, such as line plot, histogram, monthly means
+#==============================================================================
+# Uses modules:
+# datetime, numpy, pandas, plot.ly, open_DEFRA_csv
+from datetime import datetime, timedelta
+import pandas as pd
+import numpy as np
+import open_DEFRA_csv.py
+import plotly.plotly as py
+import plotly.graph_objs as go
+#==============================================================================
+
+def line_plot(arg):
+    pass
+
+def wind_rose_plot(arg):
+    pass
+
+def species_histogram(arg):
+    pass
+
+def AQ_limits(arg):
+    pass
+
+
+
+if __name__ == '__main__':
+    filename  = 'Example_Data/' \
+                    + 'edinburgh_st_leonards_2015_2017.csv'
+
+## ============================================================================
+## END OF PROGAM
+## ============================================================================
