a4619d9f2543ea5c59109d4d417386a4f15d97fc
dpfinch <d.finch@ed.ac.uk>
2017-11-02 12:18:51 +0000

    Created skeleton module which will become library of AQ limits for different species

---
+++ AQ_limits.py
@@ -0,0 +1,21 @@
+#==============================================================================
+# Do simple plots through plot.ly, such as line plot, histogram, monthly means
+# Function names:
+#   lineplot()
+#   wind_rose_plot()
+#   species_histogram()
+#==============================================================================
+# Uses modules:
+# None
+#==============================================================================
+
+def AQ_limits(arg):
+    """
+        Provides the safe limits & air quality aims for a given species.
+    """
+    pass
+
+
+## ============================================================================
+## END OF PROGAM
+## ============================================================================
