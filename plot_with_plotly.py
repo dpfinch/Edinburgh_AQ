26d080ca2d5dd2b53e2bcffc932b8613ebb53391
dpfinch <d.finch@ed.ac.uk>
2017-11-06 15:46:16 +0000

    Can now send timeseries plots to plot.ly (if you've got an account)

--- plot_with_plotly.py
+++ plot_with_plotly.py
@@ -1,7 +1,7 @@
 #==============================================================================
 # Do simple plots through plot.ly, such as line plot, histogram, monthly means
 # Function names:
-#   lineplot()
+#   timeseries_plot()
 #   wind_rose_plot()
 #   species_histogram()
 #   monthly_box_plots()
@@ -16,7 +16,7 @@ import plotly.plotly as py
 import plotly.graph_objs as go
 #==============================================================================
 
-def line_plot(species,filename = 'ExampleData', average = 'None',
+def timeseries_plot(species='None',filename = 'ExampleData', average = 'None',
     verfied = True):
     """
         Produces a simple line plot of concentration against time.
@@ -35,13 +35,28 @@ def line_plot(species,filename = 'ExampleData', average = 'None',
     """
     # Get the data for the species required. Also include the filename
     # if the filename is provided - use example data if not.
-    species_data = source_AQ_data.select_one_variable(species, filename)
+    # Also returns variable name (this might be changed slightly from user input)
+    species_data, variablename = source_AQ_data.select_one_variable(species, filename)
 
     # If just using verfied data then purge unverified data
     if verfied:
-        species_data = source_AQ_data.purge_unverified(species_data)
+        species_data = source_AQ_data.purge_unverified(species_data, variablename)
 
-    data = [x = species_data.index, y = species_data[species]]
+    # Set the data in a format the plot.ly needs to work
+    data = go.Scatter(x = species_data.index, y = species_data[variablename],
+        mode = 'markers')
+    data = [data]
+
+    # Set the layout for the plot.ly graph
+    layout = go.Layout(
+        title = 'Concentration of ' + variablename + ' at Edinburgh St leonards',
+        xaxis = dict(title = 'Date and Time'),
+        yaxis = dict(title = variablename + ' ('+species_data['Unit'][0]+')'))
+
+    # Set the filename & combine data and layout
+    filename = '%s_timeseries' % variablename
+    fig = go.Figure(data = data, layout = layout)
+    py.plot(fig,filename = filename)
 
     pass
 
